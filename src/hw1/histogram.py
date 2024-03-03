814ecb3e73ad98ec463d07f1aeba54dd9d9339e3
KB-83 <kajal.baghestani@gmail.com>
2024-03-03 22:23:30 +0330

    3 figures added and tex file updated

--- src/hw1/histogram.py
+++ src/hw1/histogram.py
@@ -1,12 +1,14 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
-# question 3 part a
+# Your data
 data = [180, 176, 176, 175, 173, 173, 172, 171, 170, 169, 168, 166, 165, 193,
         191, 189, 187, 185, 185, 185, 184, 183, 182, 182, 181, 181]
 
-data.sort()
+# Define custom bin edges
+bin_edges = np.arange(160, 196, 5)  # Start at 160, end at 185, with bin width 5
 
-plt.hist(data, bins=5, cumulative=True, edgecolor='black')
+plt.hist(data, bins=bin_edges, cumulative=True, edgecolor='black')
 
 plt.xlabel('data')
 plt.ylabel('people')
