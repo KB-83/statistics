af8d8f21b7a99958206eceeab4bac7282ca804b3
KB-83 <kajal.baghestani@gmail.com>
2024-03-06 22:28:26 +0330

    .gitignore updated

--- src/hw1/histogram.py
+++ src/hw1/histogram.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy import stats
+
 
 # Your data
 data = [180, 176, 176, 175, 173, 173, 172, 171, 170, 169, 168, 166, 165, 193,
@@ -14,3 +16,21 @@ plt.xlabel('data')
 plt.ylabel('people')
 plt.title('cumulative histogram')
 plt.show()
+
+
+
+def mean_root(data, r):
+    n = len(data)
+    if r > 0:
+        mean_root = (np.sum(np.abs(data)**r * 1/n))**(1/r)
+    elif r < 0:
+        mean_root = 1/((np.sum((1/(np.abs(data)**-r)) * 1/n))**(-1/r))
+    else:
+        mean_root = stats.gmean(data)
+    return mean_root
+
+print("Mean Root for -1:", mean_root(data,-1))
+print("Mean Root for 0:", mean_root(data,0))
+print("Mean Root for 1:", mean_root(data,1))
+print("Mean Root for 2:", mean_root(data,2))
+
